def7325044119010783d76d1d9ea8ebe23554087
DinaSiagian <143679301+DinaSiagian@users.noreply.github.com>
2024-02-06 21:36:31 +0700

    coba

--- t03_01.c
+++ t03_01.c
@@ -1,45 +1,27 @@
 // 12SS23009 - DINA SIAGIAN
 
 #include <stdio.h>
-#include <stdlib.h> 
-#include <string.h> 
-#define MAX_SIZE 100
 
 int main() {
-    int arr[MAX_SIZE];
-    int smallest, largest;
-    char input[20]; 
+    int num, smallest, largest;
 
+    for (int i = 0; i < 9; i++) {
+        scanf("%d", &num);
 
-    int n = 0;
-    while (1) {
-        scanf("%s", input);
-        
-        if (strcmp(input, "---") == 0) {
+        if (num == 0) {
             break;
         }
 
-        arr[n] = atoi(input);
-        n++;
-        
-        if (n >= MAX_SIZE) {
-            printf("Batas array tercapai. Mengakhiri masukan nilai.\n");
-            break;
-        }
-    }
-
-    if (n == 0) {
-        printf("Tidak ada nilai yang dimasukkan.\n");
-        return 0;
-    }
-
-    smallest = largest = arr[0];
-    for (int i = 1; i < n; i++) {
-        if (arr[i] < smallest) {
-            smallest = arr[i];
-        }
-        if (arr[i] > largest) {
-            largest = arr[i];
+        if (i == 0) {
+            smallest = num;
+            largest = num;
+        } else {
+            if (num < smallest) {
+                smallest = num;
+            }
+            if (num > largest) {
+                largest = num;
+            }
         }
     }
 
--- t03_02.c
+++ t03_02.c
@@ -1,11 +1,44 @@
-// NIM - NAMA
+// 12S23009 - DINA SIAGIAN
 
 #include <stdio.h>
+#include <math.h>
 
-int main(int _argc, char **_argv)
-{
+int main() {
+    int num, smallest, largest;
+    int total = 0; // variable to store the total of all numbers
+    int count = 0; // variable to count the number of inputs
 
-  // your code here
+    for (int i = 0; i < 9; i++) {
+        scanf("%d", &num);
 
-  return 0;
-}
+        if (num == 0) {
+            break;
+        }
+
+        total += num; // add the number to the total
+        count++; // increment the count
+
+        if (i == 0) {
+            smallest = num;
+            largest = num;
+        } else {
+            if (num < smallest) {
+                smallest = num;
+            }
+            if (num > largest) {
+                largest = num;
+            }
+        }
+    }
+
+    if (count > 0) {
+        double average = round((double)total / count * 100) / 100; // calculate the average
+        printf("%d\n", smallest);
+        printf("%d\n", largest);
+        printf("%.2f\n", average); // print the average
+    } else {
+        printf("No numbers were entered.\n");
+    }
+
+    return 0;
+} 
