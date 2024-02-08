579f835c83145229495cc09a67ce79a3e48a5f5b
DinaSiagian <143679301+DinaSiagian@users.noreply.github.com>
2024-02-08 17:22:08 +0700

    test

--- t03_01.c
+++ t03_01.c
@@ -3,30 +3,31 @@
 #include <stdio.h>
 
 int main() {
-    int num, smallest, largest;
+    int n, i;
+    int numbers[100]; // Array to hold the numbers
 
-    for (int i = 0; i < 9; i++) {
-        scanf("%d", &num);
+    scanf("%d", &n);
 
-        if (num == 0) {
-            break;
+    for(i = 0; i < n; i++) {
+        scanf("%d", &numbers[i]);
+    }
+
+    // Assume the first number is the smallest and largest
+    int min = numbers[0];
+    int max = numbers[0];
+
+    // Loop through the array to find the actual min and max
+    for(i = 1; i < n; i++) {
+        if(numbers[i] < min) {
+            min = numbers[i];
         }
-
-        if (i == 0) {
-            smallest = num;
-            largest = num;
-        } else {
-            if (num < smallest) {
-                smallest = num;
-            }
-            if (num > largest) {
-                largest = num;
-            }
+        if(numbers[i] > max) {
+            max = numbers[i];
         }
     }
 
-    printf("%d\n", smallest);
-    printf("%d\n", largest);
+    printf("%d\n", min);
+    printf("%d\n", max);
 
     return 0;
 } 
